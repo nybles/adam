931819f2710693382146133e17ed3544912ac9cf
aswinzz <iit2016106@iiita.ac.in>
2017-03-14 11:31:37 +0530

    Update loader.js

--- static/js/loader.js
+++ static/js/loader.js
@@ -3,5 +3,4 @@ $(window).load(function() { // makes sure the whole site is loaded
     $("#preloader").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
 })
 
-// Animated CSS preloader from http://tobiasahlin.com/spinkit/
-// Excellent preloader example found here: http://ismet.me/index.html
+
