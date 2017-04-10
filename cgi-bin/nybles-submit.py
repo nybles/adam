bac1f50bbfbed8c47d54fba9601d0caaed9b998a
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-04-10 19:38:40 +0530

    add extra file to store metadata

--- cgi-bin/nybles-submit.py
+++ cgi-bin/nybles-submit.py
@@ -4,11 +4,32 @@ import cgi, cgitb
 import json
 import os
 from sys import exit
+from time import time
+from hashlib import md5
 
 # for debugging
 cgitb.enable()
 
 target_directory = "/data/geekhaven/nybles-submissions"
+submissions_db =   "/data/geekhaven/nybles-submissions/submissions.json"
+
+
+def getSubmissions():
+    d = {}
+    if (os.path.exists(submissions_db)):
+        with open(submissions_db) as f:
+            d = json.loads(f.read())
+    return d
+
+
+def saveSubmissions(dic):
+    with open(submissions_db, 'w+') as f:
+        f.write(json.dumps(dic))
+
+
+def getSubmissionId(content):
+    return md5(content.encode()).hexdigest()
+
 
 form = cgi.FieldStorage()
 try:
@@ -29,15 +50,22 @@ try:
         "title": form.getvalue("title"),
         "author": form.getvalue("author"),
         "categories": list(map(lambda cat: cat.strip(),
-                            form.getvalue("categories").split(","))),
-        "content": form.getvalue("content")
+                            form.getvalue("categories").split(",")))
     }
+    content = form.getvalue("content")
 
     # form submitted
-    filename = response["author"] + '-' + response["title"] + ".md"
+    encoding = response["author"] + '-' + response["title"] + ".json"
+    filename = getSubmissionId(encoding)+".md"
+    response["filename"] = filename
     target = os.path.join(target_directory, filename)
+    currentDb = getSubmissions()
+    currentDb = {}
+    currentDb[filename] = response
+    saveSubmissions(currentDb)
+
     with open(target, 'w+') as f:
-        f.write(json.dumps(response, indent=4))
+        f.write(content)
 except Exception as e:
     print ("Content-type: text/plain")
     print ()
